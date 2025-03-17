7270cf3bc6b4279bcd7f272735379a733fbdead6
Alexander López Sánchez <Alopezs@ucenfotec.ac.cr>
2025-03-17 17:49:44 -0600

    Perfil Usuario, detalles navbar

--- Front/src/presentation/components/Profile.tsx
+++ Front/src/presentation/components/Profile.tsx
@@ -3,12 +3,19 @@ import React from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUniversity, faStar, faComments } from "@fortawesome/free-solid-svg-icons";
+import { useSession } from "next-auth/react";
 
 const Profile = () => {
+  const { data: session } = useSession();
+
+  const userProfileImage = session?.user?.image || "https://randomuser.me/api/portraits/women/43.jpg";
+  const userName = session?.user?.name || "Usuario Desconocido";
+  const userEmail = session?.user?.email || "No disponible";
+
   return (
       <main className="profile-page bg-[#fdfefe] dark:bg-gray-900 min-h-screen">
         {/* Sección de Portada */}
-        <section className="relative w-full p-4 bg-gradient-to-r from-blue-300 to-indigo-500 dark:from-blue-700 dark:to-indigo-800">
+        <section className="relative w-full p-4 bg-gradient-to-r from-blue-400 to-blue-700 dark:from-gray-900 dark:to-gray-900">
           <div className="absolute inset-0 flex justify-center items-center">
             <div className="absolute w-32 h-32 bg-white opacity-10 rounded-full top-10 left-16 dark:bg-gray-600"></div>
             <div className="absolute w-24 h-24 bg-white opacity-10 rounded-full top-20 right-20 dark:bg-gray-600"></div>
@@ -26,19 +33,19 @@ const Profile = () => {
               <div className="relative w-44 h-44 mx-auto mb-4 sm:w-32 sm:h-32">
                 <Image
                     alt="Perfil"
-                    src="https://randomuser.me/api/portraits/women/43.jpg"
-                    layout="fill"
-                    objectFit="cover"
+                    src={userProfileImage}
+                    width={176}
+                    height={176}
                     className="shadow-xl rounded-full border-4 border-white dark:border-gray-700"
                 />
               </div>
 
               {/* Información del Usuario */}
               <h3 className="text-3xl font-semibold text-[#002855] dark:text-white">
-                Ana Morales
+                {userName}
               </h3>
               <p className="text-lg text-blue-400 dark:text-blue-300 mt-2">
-                Ejemplo@ucenfotec.ac.cr
+                {userEmail}
               </p>
               <p className="text-lg text-[#002855] dark:text-white mt-2 font-medium">
                 Estudiante
@@ -56,7 +63,7 @@ const Profile = () => {
 
                 {/* Calificaciones */}
                 <div className="p-6 bg-[#fdfefe] dark:bg-blue-700 rounded-xl shadow-md w-48 text-center">
-                  <span className="text-3xl font-bold text-[#002855] dark:text-white">5</span>
+                  <span className="text-3xl font-bold text-[#002855] dark:text-white">0</span>
                   <div className="text-md text-blue-400 dark:text-blue-300 flex items-center justify-center mt-2">
                     <FontAwesomeIcon icon={faStar} className="mr-2"/> Calificaciones
                   </div>
@@ -64,7 +71,7 @@ const Profile = () => {
 
                 {/* Comentarios */}
                 <div className="p-6 bg-[#fdfefe] dark:bg-blue-700 rounded-xl shadow-md w-48 text-center">
-                  <span className="text-3xl font-bold text-[#002855] dark:text-white">10</span>
+                  <span className="text-3xl font-bold text-[#002855] dark:text-white">0</span>
                   <div className="text-md text-blue-400 dark:text-blue-300 flex items-center justify-center mt-2">
                     <FontAwesomeIcon icon={faComments} className="mr-2"/> Respuestas
                   </div>
