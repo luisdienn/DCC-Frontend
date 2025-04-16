7c0de302e8f9fdfa7fb4df1488877096ba625a4b
luisdienn <ldienb@ucenfotec.ac.cr>
2025-04-16 10:41:59 -0600

    Update next.config.ts

--- Front/next.config.ts
+++ Front/next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Ignora errores de ESLint en producción
+  },
   images: {
-    domains: ["randomuser.me", "developers.google.com", "t4.ftcdn.net", "https://images.vexels.com", "lh3.googleusercontent.com"], // Agregamos el dominio permitido
+    domains: [
+      "randomuser.me",
+      "developers.google.com",
+      "t4.ftcdn.net",
+      "images.vexels.com", // ❗️ Quité el "https://" porque solo debe ir el dominio
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
